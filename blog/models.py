bbc4039fe180c5a410cdc0dc145843ef0861b883
JonEast87 <j.d.east@icloud.com>
2024-02-18 15:13:32 -0700

    first create functionality, templates/blog will be temporary while I work out update and delete before setting up a seperate front end with react

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,20 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    class Meta:
+        # This is to correct django's defualt behaviour of appending an 's' to the end of classes for plural spelling
+        verbose_name_plural = "categories"
+
+    # Set to for better string representation
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField("Category", related_name="posts")
+
+    def __str__(self):
+        return self.title
