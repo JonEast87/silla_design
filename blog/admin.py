f579a20886e854bffdd01deffe06ad3c5df736a6
JonEast87 <j.d.east@icloud.com>
2024-02-18 12:10:35 -0700

    built basic models to hammer out the structure of the site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+# File imports
+from blog.models import Category, Post
+
 # Register your models here.
+# Passing with default configurations is good enough at this moment
+class CategroyAdmin(admin.ModelAdmin):
+    pass
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+# Registering the actual models with the Admin class
+admin.site.register(Category, CategroyAdmin)
+admin.site.register(Post, PostAdmin)
